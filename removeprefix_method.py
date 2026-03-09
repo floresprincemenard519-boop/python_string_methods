f16e148505380e5ba72a3475ca073a842d067c43
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 15:57:15 +0800

    Cleaned and finalized the code for removeprefix.py.

--- removeprefix_method.py
+++ removeprefix_method.py
@@ -1,9 +1,9 @@
 string = "   What a beautiful world! Helloooooo!"
 prefix_to_remove = "   W"
+
 # how removeprefix() works
 print(string.removeprefix('   W'))
 
 # same functionality without using removeprefix()
-# find the index to remove and exclude it from the string
 string = string[len(prefix_to_remove):]
 print(string)
