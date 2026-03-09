cc00bee83bc9d84df02d9711de75ba64c78bd8f5
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 17:51:09 +0800

    Made the code not using swapcase method. I used other methods like lower()  and upper()

--- swapcase_method.py
+++ swapcase_method.py
@@ -2,6 +2,15 @@ large_string = "I SHOULD BE SMALL"
 small_string = "i should be large"
 
 # how swapcase() works
-print(large_string.swapcase())
-print(small_string.swapcase())
+# print(large_string.swapcase())
+# print(small_string.swapcase())
 
+# how swapcase() works without using swapcase() method
+def swapcase(string):
+    if string.isupper():
+        string = string.lower()
+    elif string.islower():
+        string = string.upper()
+    print(string)
+
+swapcase(large_string)
