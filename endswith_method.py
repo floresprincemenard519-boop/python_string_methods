589a22072067bb255fb69fc4527045479a176ba1
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 16:47:26 +0800

    Added more examples.

--- endswith_method.py
+++ endswith_method.py
@@ -1,10 +1,11 @@
 email_address = "me.@gmail.com"
+second_email_address = "you.@gmail.com"
 
 # how endswith() works
 print(f"Is '{email_address}' a gmail address? {email_address.endswith('@gmail.com')}")
 
 # how endswith() works without endswith() method
-if "@gmail.com" in email_address[-10:]:
-    print(f"Is '{email_address}' a gmail address? True")
+if "@gmail.com" in second_email_address[-10:]:
+    print(f"Is '{second_email_address}' a gmail address? True")
 else:
-    print(f"Is '{email_address}' a gmail address? False")
+    print(f"Is '{second_email_address}' a gmail address? False")
