f51459a5e638617bcbedfb727a063ad920e1b8e2
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 16:25:02 +0800

    Did some test and getting some errors.

--- isupper_method.py
+++ isupper_method.py
@@ -2,3 +2,9 @@ string = "abcdefg hi! eheheheh"
 
 # how isupper() works
 print(string.isupper())
+
+for char in string:
+    if char > string[string.index(char)+1]:
+        print(char)
+
+print(string.index("a"))
