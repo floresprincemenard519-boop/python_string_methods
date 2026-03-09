c7f19bf8f037e4dc956a45f6974539dfc616761d
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 18:15:26 +0800

    Encountered a error and fixed the code. it didnt work the first time.

--- title_mothod.py
+++ title_mothod.py
@@ -1,11 +1,11 @@
-string = "i am a title of a book"
+string = " i am a title of a book"
 
 # how title() method works
-print(string.title())
+# print(string.title())
 
 # how title() method works with title() method
 for char in string:
-    if string.index(char)-1 == ' ':
+    if string[string.index(char)-1] == " ":
         print(char.upper(), end="")
     else:
         print(char, end="")
