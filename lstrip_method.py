b1b6e875a6d72b7e628a4c6a466b62c5ad9d00a0
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 14:44:30 +0800

    Cleaned the code for lstrip() method.

--- lstrip_method.py
+++ lstrip_method.py
@@ -1,15 +1,9 @@
-# lstrip() example function
 string = "   What a beautiful world! Helloooooo!"
-# print(string.lstrip())
 spaceless_string = ""
 non_space_detected = False
-# remove spaces without using lstrip()
 
-# check if the characters in string is space or not
-# if character is space then remove it from the string
-# if encountered a non space dont remove spaces further 
 for char in string:
-    if char != " ":
+    if char != " " or non_space_detected:
         spaceless_string += char
         non_space_detected = True
 
