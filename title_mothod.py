fe8a91279dd75f25043381e2c01cf3343e760e77
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 18:07:36 +0800

    Added a pseudo code to organize what to do.

--- title_mothod.py
+++ title_mothod.py
@@ -3,3 +3,11 @@ string = "i am a title of a book"
 # how title() method works
 print(string.title())
 
+# how title() method works with title() method
+for char in string:
+    pass
+
+# check each letter
+# check if that letter is the beginning of the word
+# change to cappital if it is the beginning of the word
+# print all the letters together
