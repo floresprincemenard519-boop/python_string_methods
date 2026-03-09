6db136d1c3340d7d9108eca9bbf8ad6cef819550
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-09 17:20:43 +0800

    Finished the center() method.

--- center_method.py
+++ center_method.py
@@ -2,3 +2,10 @@ string = "I should be in the center."
 
 # how center() works
 print(string.center(50,"-"))
+
+# how center() works without center() method
+if len(string) != 50:
+    padding = (50 - len(string)) //2
+    print('-' * padding + string + '-' * padding)
+else:
+    print(string)
