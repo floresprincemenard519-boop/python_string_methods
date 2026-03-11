37b7ffd76dec6395587bc0e5cfd8996237a27d61
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 20:26:33 +0800

    Finished and cleaned the code.

--- even_numbers_counter.py
+++ even_numbers_counter.py
@@ -1,3 +1,9 @@
-# get 10 numbers
-# count all even
-# print even num total
+total_even = 0
+
+for num in range(10):
+    numbers = float(input("Please input a number: "))
+
+    if numbers % 2 == 0:
+        total_even += 1
+
+print(total_even)
