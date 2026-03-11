74f8f7aebba1524427d10bb7f25ca10ddc0def78
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 03:24:46 +0800

    finished the code. ill not remove pseudocode cause it takes time and more commits.

--- upper_without_function.py
+++ upper_without_function.py
@@ -1,2 +1,9 @@
 # get a string
 # if chr is lower make it upper
+string = "This string should be in UPPer Case."
+
+for char in string:
+    if not char.isupper():
+        print(char.swapcase(), end="")
+    else:
+        print(char, end="")
