bdbc269bb037439f35115649f97444c8e289760f
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 20:50:05 +0800

    This is the idea i was talking about. its using while loop instead of range.

--- numbers_between_two_nums.py
+++ numbers_between_two_nums.py
@@ -1,2 +1,9 @@
 # get two nums
 # get the numbers in the middle using range or maybe i can use a while loop
+first_number = float(input("Please input a number: "))
+second_number = float(input("please input another number: "))
+
+if first_number > second_number:
+    while first_number != second_number:
+        print(first_number)
+        first_number -= 1
