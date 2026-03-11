438f24663267cf1a659d51996bba5eb9435b8aa6
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 13:50:24 +0800

    Added another way to add multiple numbers inrange of 5. You can change the range to the amount of numbers you need.

--- sum_of_multiple_numbers.py
+++ sum_of_multiple_numbers.py
@@ -6,3 +6,11 @@ second_number = float(input("Give me another: "))
 third_number = float(input("Another: "))
 # sum all of them
 print(first_number + second_number + third_number)
+
+list = []
+
+for number in range(5):
+    number = float(input("Give me a number: "))
+    list.append(number)
+    
+print(sum(list))
