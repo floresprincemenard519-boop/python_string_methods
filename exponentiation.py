41f4e9ee04baebe667c9285a4bd0782d0719bc56
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 13:39:29 +0800

    Finished and cleaned the code.

--- exponentiation.py
+++ exponentiation.py
@@ -1,6 +1,4 @@
-# get the first number
 base = float(input("Give me the base: "))
-# get the second number
 exponent = float(input("Give me the exponent: "))
-# print the exponent 
+
 print(base ** exponent)
