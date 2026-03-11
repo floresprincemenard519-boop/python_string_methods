e078780395247ff6018e469aaa5cac0c06f77438
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 13:13:22 +0800

    Followed the pseudocode.

--- sum_of_two_numbers.py
+++ sum_of_two_numbers.py
@@ -1,4 +1,6 @@
 # get first number
-
+first_number = float(input("Give me the first number to add: "))
 # get second number
+second_number = float(input("Give me another number to add with the first: "))
 # add two numbers
+print(first_number + second_number)
