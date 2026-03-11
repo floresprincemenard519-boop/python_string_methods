3046d19add738e2c708c7bf38ebe62d077aaa1c6
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 13:38:56 +0800

    Followed the pseudocode and tested it with some numbers.

--- exponentiation.py
+++ exponentiation.py
@@ -1,4 +1,6 @@
 # get the first number
- 
+base = float(input("Give me the base: "))
 # get the second number
+exponent = float(input("Give me the exponent: "))
 # print the exponent 
+print(base ** exponent)
