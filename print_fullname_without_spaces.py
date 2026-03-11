1573b1f6c130cc7f4369bd013cca7513b37f755e
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 00:32:40 +0800

    Finished and cleaned the code.

--- print_fullname_without_spaces.py
+++ print_fullname_without_spaces.py
@@ -1,8 +1,5 @@
-# get the input for the name 
-# remove spaces from the beginning
-# print result
-
 fullname = input("Please input your full name.\n")
 fullname = "     " + fullname
+
 print(fullname)
 print(fullname.lstrip(" "))
