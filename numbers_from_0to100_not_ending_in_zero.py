053a56bce5d2e74cbf89fc044a0204257799cb61
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 19:10:26 +0800

    followed the pseudocode and found a loop.

--- numbers_from_0to100_not_ending_in_zero.py
+++ numbers_from_0to100_not_ending_in_zero.py
@@ -1,4 +1,7 @@
 # for loop until 100
+for numbers in range(0,100):
 # check if ending in 0
+    if numbers % 10 != 0:        
 # if ending in 0 dont print
+        print(numbers, end=" ")
 # if no print
