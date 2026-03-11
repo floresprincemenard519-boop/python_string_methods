9b1f2ce26d5ee8634735299323d1fd520cd9623c
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 13:28:09 +0800

    Finished and cleaned the code of comments.

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,6 +1,4 @@
-# get the first number
 multiplicand = float(input("Give me the multiplicand: "))
-# get the second number
 multiplier = float(input("Give me the multiplier: "))
-# print the product of the two numbers
+ 
 print(multiplicand * multiplier)
