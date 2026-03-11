268934649f010688177d39f3d16ec6fee3ed6a1f
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 19:02:46 +0800

    Finished the code and will clean lateron.

--- even_numbers_printer.py
+++ even_numbers_printer.py
@@ -1,2 +1,5 @@
 # use for loop to check numbers till 100 
+for number in range(0,101):
+    if number % 2 == 0:
 # if even print if not don't print 
+        print(number, end=" ")
--- odd_number_counter.py
+++ odd_number_counter.py
@@ -3,7 +3,7 @@ odd_counter = 0
 for number in range(5):
     number = float(input("Give me a number: "))
 
-    if number %2 != 0:
+    if number % 2 != 0:
         odd_counter += 1
 
 print(odd_counter)
