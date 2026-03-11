8bae84e01cd895d040429aba68ddd03c459f5076
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 12:59:44 +0800

    Cleaned the code.

--- bigger_number_program.py
+++ bigger_number_program.py
@@ -1,9 +1,7 @@
-# get two numbers
 first_number = float(input("Give me a number: "))
 second_number = float(input("Give me another number: "))
-# which of the two numbers is bigger 
+
 if first_number > second_number:
-# print bigger number 
     print(f"The bigger number is {first_number}.")
 elif second_number > first_number:
     print(f"The bigger number is {second_number}.")
