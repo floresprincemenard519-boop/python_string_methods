96c7284637e8bad22082be6d922b87dcff899caf
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 13:05:37 +0800

    Cleaned the code for equal_number_determiner.py

--- equal_number_determiner.py
+++ equal_number_determiner.py
@@ -1,8 +1,5 @@
-# get first number
 first_number = float(input("Give me a number: "))
-# get second number
 second_number = float(input("Give me another number: "))
-# ask if equal
+
 if first_number == second_number:
-# if equal print "Equal"
     print("Equal")
