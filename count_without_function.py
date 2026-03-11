3adc41bc799f9d38ce215a4411c7abcd2945f721
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 04:12:19 +0800

    cleaned and finshed the code

--- count_without_function.py
+++ count_without_function.py
@@ -2,3 +2,11 @@
 # count a char in string
 # print count
 
+string = "This string should contain 4 t's."
+t_counter = 0
+
+for char in string:
+    if char == 't' or char == 'T':
+        t_counter += 1
+
+print(t_counter)
