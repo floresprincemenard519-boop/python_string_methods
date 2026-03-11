d5a7c34f6341480302ade53033668c6739327726
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-11 21:49:59 +0800

    Fixed the code logic and cleaned it.

--- duplicate_remover.py
+++ duplicate_remover.py
@@ -1,11 +1,11 @@
-# get 10 numbers 
-# store numbers in a list 
-# if a number is in the list remove it and just retain the non duplicated ones
 list = []
+
 for num in range(10):
     numbers = float(input("Input a number: "))
-    list.append(numbers) 
     if numbers in list:
         list.remove(numbers)
+    else:
+        list.append(numbers) 
 
-print(list)
+for num in list:
+    print(num, end=" ")
