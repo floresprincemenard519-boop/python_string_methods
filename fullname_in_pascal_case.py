219f98845cfcb756aca7dba2934214201e164483
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 01:47:42 +0800

    problem with some numbers remaining in uppercase

--- fullname_in_pascal_case.py
+++ fullname_in_pascal_case.py
@@ -8,6 +8,8 @@ fullname = input("Please input your fullname: ")
 for index, char in enumerate(fullname):
     if index == 0 or fullname[index - 1] == ' ':
         print(char.upper(), end="")
+    elif fullname[index - 1] == fullname[index - 1].isupper:
+        print(char.lower(), end="")
     elif char == ' ':
         print('', end='')
     else:
