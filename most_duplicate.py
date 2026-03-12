01bc7e4f0b74c18811ee1f4183b638c98bec8d2a
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-03-12 16:58:27 +0800

    scrapped the idea and tried to use just a dictionary

--- most_duplicate.py
+++ most_duplicate.py
@@ -2,3 +2,10 @@
 # get numbers
 # get the highest num of duplicates
 # print the number not the count
+list = {}
+while True:
+    try:
+        numbers = float(input("Please input a number: "))
+        list[numbers] = list.get(numbers,0) + 1
+    except ValueError:
+        
